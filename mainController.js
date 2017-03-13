0d102087346b4c4c15533e27350470f74d9cf9d6
covega <john5morgan29@gmail.com>
2017-03-12 17:46:09 -0700

    basic route structure. Can now succesfully run and navigate to blank pages via urls

---
+++ mainController.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var cs247App = angular.module('cs247App', ['ngRoute']);
+
+cs247App.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/chat', {
+                templateUrl: 'components/chat/chatTemplate.html',
+                controller: 'ChatController'
+            }).
+            when('/profile', {
+                templateUrl: 'components/profile/profileTemplate.html',
+                controller: 'ProfileController'
+            }).
+            when('/profile-creation', {
+                templateUrl: 'components/profile-creation/profile-creationTemplate.html',
+                controller: 'Profile-CreationController'
+            }).
+            when('/register', {
+                templateUrl: 'components/register/registerTemplate.html',
+                controller: 'RegisterController'
+            }).
+            when('/select', {
+                templateUrl: 'components/select/selectTemplate.html',
+                controller: 'SelectController'
+            }).  
+            otherwise({
+                redirectTo: '/register'
+            });
+    }]);
+
+cs247App.controller('MainController', ['$scope', '$rootScope', '$location',
+  function ($scope, $rootScope, $location) {
+        $scope.main = {};
+
+
+}]);
+
+
