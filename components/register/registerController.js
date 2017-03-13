e9bcddd7cabe7b8048415c0927f6c4d896d4e87c
covega <john5morgan29@gmail.com>
2017-03-12 21:26:35 -0700

    basic functionality for registration progression

--- components/register/registerController.js
+++ components/register/registerController.js
@@ -5,8 +5,37 @@ cs247App.controller('RegisterController', ['$scope',
     /*
      *
      */
-     $scope.main = {};    
+     $scope.main = {};
+     $scope.main.user = {};
+
+     $scope.step = 0;
+
+     $scope.progressFromStart = function(currPatient) {
+          $scope.main.user.currPatient = currPatient;
+          $scope.step++;
+     };
+
+     $scope.progressFromAge = function(maxAge) {
+          $scope.main.user.maxAge =  maxAge;
+          $scope.step++;
+     };
+
+     $scope.progressFromType = function(type) {
+          //$scope.main.user.surgeryType =  type;
+          $scope.step++;
+     };     
+
+     $scope.progressFromAdditionalSurgeries = function(surgeries) {
+          $scope.step++;
+     };    
+
+     $scope.progressFromRecovery = function(recoveryPeriod) {          
+          $scope.step++;
+     };    
+
+     $scope.progressFromHormone = function(hormone) {          
+          $scope.step++;
+     };             
 
      /* assemble a json object with each click */
-     
   }]);
