0e7655e4d980e6e94e30ea983fc9a4fc74fac177
covega <john5morgan29@gmail.com>
2017-03-12 19:57:30 -0700

    added buttons with content, but no styling

---
+++ components/register/registerController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+cs247App.controller('RegisterController', ['$scope',
+  function ($scope) {
+    /*
+     *
+     */
+     $scope.main = {};    
+
+     /* assemble a json object with each click */
+     
+  }]);
