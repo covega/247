ffa7a9f274f8f2341f10f7975034385532c232d2
covega <john5morgan29@gmail.com>
2017-03-17 16:35:31 -0700

    added some stickers

--- components/chat/chatController.js
+++ components/chat/chatController.js
@@ -11,6 +11,8 @@ cs247App.controller('ChatController', ['$scope', '$location', '$rootScope',
 
    $scope.suggestions = ["What should I be eating?", "Are there resources you'd recommend?", "What do you wish you knew?"];
 
+   $scope.stickers = ["http://www.stickermotions.com/wp-content/uploads/2014/12/2812144885022352431.png", "http://www.stickermotions.com/wp-content/uploads/2014/12/2812144884925685774.png", "http://www.stickersort.com/wp-content/uploads/2014/12/669Sortlist-Facebook-Stickers.png", "http://www.stickersort.com/wp-content/uploads/2014/12/671Sortlist-Facebook-Stickers.png", "https://s-media-cache-ak0.pinimg.com/564x/cd/de/75/cdde758c30c75db094ef748a15883367.jpg"];
+
     var path = $location.path();
     var id = parseInt(path.substring(path.length-1));
     $scope.partner = $rootScope.main.mentors[id-1];
